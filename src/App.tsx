48bb1ddeaf860d8cc885f2f168eb39569b9cdd74
Eddie Kusturica <eddkus@outlook.com>
2023-03-22 15:14:43 +0100

    Initial commit of the application

---
+++ src/App.tsx
@@ -0,0 +1,17 @@
+import './App.css'
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1 className="main-logo">Eddie's Moving Company</h1>
+      <div className="card">
+        <button>
+          Make an Order
+        </button>
+      </div>
+      <p className="read-the-docs">
+        Quick and Clean Moving Service
+      </p>
+    </div>
+  )
+}
